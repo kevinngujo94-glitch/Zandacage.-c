52ad0131c8a5092e69160e7c29eefee0072bb9de
kevinngujo94-glitch <kevinngujo94@gmail.com>
2025-09-22 15:38:58 +0300

    Add files via upload

---
+++ zandacage. c.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+// simple c programming 
+/*
+Name:Kevin Ngujo 
+Reg No:CT100/G/26118/25
+Description :semester units
+*/
+ 
+ int main ()
+ {
+     printf("Analogue electronics\n"); 
+     printf("computer system and organisation\n");
+     printf("fundamentals of ICT\n");
+     printf("Essential of accounting\n");
+     printf("introduction to computer applications \n");
+     printf("communication and information literacy skills\n");
+     printf("mathematics for science\n");
+     printf("introduction to programming and algorithms\n");
+     return 0;
+ }   
