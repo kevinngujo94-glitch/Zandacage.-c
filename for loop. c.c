fb08555b3b55627dacc4f175ab27625b8a7f6914
kevinngujo94-glitch <kevinngujo94@gmail.com>
2025-10-23 23:55:43 +0300

    Add files via upload

---
+++ for loop. c.c
@@ -0,0 +1,14 @@
+/*
+Name;kevin Ngujo Mwagambo 
+reg no;CT100/G/26118/25
+Description;program to print 100-50
+*/
+
+#include<stdio.h>
+int main(){
+//for(start;stop;step)}
+  for (int i=100;i>=50;i--){
+  printf("%d\n",i);}
+  
+  return 0;
+ }
